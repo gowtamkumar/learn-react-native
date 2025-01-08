fb40a5405034aac01665d425e3845da227ff112d
Gowtam kumar <gowtampaul0@gmail.com>
2025-01-08 20:56:00 +0600

    stack route

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -9,7 +10,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>i am learn React native.</Text>
+      <Link href={'/about'}>Go to About</Link>
     </View>
   );
 }
