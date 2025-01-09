0f752958bf5276654e80536a575e29a52870a495
Gowtam kumar <gowtampaul0@gmail.com>
2025-01-09 22:17:19 +0600

    learn react native

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,10 +4,11 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name='(drawer)'  options={{headerShown: false}} />
-      <Stack.Screen name='(tabs)'  options={{headerShown: false}} />
-      <Stack.Screen name="index" />
-      <Stack.Screen name="about" />
+      <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+      <Stack.Screen name='(drawer)' options={{ headerShown: false }} />
+      <Stack.Screen name="contact" />
+      <Stack.Screen name="login" />
+      <Stack.Screen name="signup" />
     </Stack>
   );
 }
